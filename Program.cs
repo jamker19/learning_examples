dc905012119d000419ac80946a9662a6dacc33ec
jamker19 <48854362+jamker19@users.noreply.github.com>
2023-04-16 05:55:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_girilen_5_sayi_toplam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int n=0,toplam=0;
+            for(int i=1; i<=5; i++)
+            {
+                Console.Write(i+".Sayıyı giriniz:");
+                n=Convert.ToInt16(Console.ReadLine());
+                toplam +=n; ;
+
+            }
+            Console.Write("Beş sayının toplamı:"+toplam);
+            Console.ReadKey();
+        }
+    }
+}
